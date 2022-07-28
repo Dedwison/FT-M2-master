f6c29f8d2f7279d17797dec1619b648e9f0503c4
Dedwison <dedwison@gmail.com>
2022-07-27 21:34:31 -0400

    HW-06_ReactIntro

--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import Card from "./Card";
 
 export default function Cards(props) {
+  /*   if (!props.cities) {
+    return <h1>No cities</h1>;
+  } */
+  //con destructuring
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
-};
+  return (
+    <div>
+      {
+        // props.cities && props.cities.map((city) =>
+        // props.cities?.map(city =>
+        props.cities ? (
+          props.cities.map((city) => (
+            <Card
+              key={city.id}
+              max={city.main.temp_max}
+              min={city.main.temp_min}
+              name={city.name}
+              img={city.weather[0].icon}
+              onClose={() => alert(city.name)}
+            />
+          ))
+        ) : (
+          <h1>No cities</h1>
+        )
+      }
+    </div>
+  );
+}
